34c851318ede9276710a8104304ce093d9abe992
Rebekah <rebekah.kokaev@hotmail.com>
2022-12-15 15:19:08 +0200

    added components for search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogBreedSearchComponent } from './dog-breed-search/dog-breed-search.component';
+import { SearchCardComponent } from './dog-breed-search/search-card/search-card.component';
+import { NameSearchComponent } from './dog-breed-search/search-card/name-search/name-search.component';
+import { TraitSearchComponent } from './dog-breed-search/search-card/trait-search/trait-search.component';
+import { BackgroundComponent } from './dog-breed-search/background/background.component';
+import { RangeInputComponent } from './dog-breed-search/search-card/trait-search/range-input/range-input.component';
+import { OptionInputComponent } from './dog-breed-search/search-card/trait-search/option-input/option-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogBreedSearchComponent,
+    SearchCardComponent,
+    NameSearchComponent,
+    TraitSearchComponent,
+    BackgroundComponent,
+    RangeInputComponent,
+    OptionInputComponent
   ],
   imports: [
     BrowserModule,
