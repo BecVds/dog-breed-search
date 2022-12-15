26fa891153c9950304faec9280bef0aafb31a0d3
Rebekah <rebekah.kokaev@hotmail.com>
2022-12-15 15:19:27 +0200

    added components for search page

---
+++ src/app/dog-breed-search/dog-breed-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-breed-search',
+  templateUrl: './dog-breed-search.component.html',
+  styleUrls: ['./dog-breed-search.component.scss']
+})
+export class DogBreedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dog-breed-search/search-card/trait-search/trait-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trait-search',
+  templateUrl: './trait-search.component.html',
+  styleUrls: ['./trait-search.component.scss']
+})
+export class TraitSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
