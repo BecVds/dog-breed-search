07a0c836a22f4d0a3aabd4c6579f269d35ab8851
Rebekah <rebekah.kokaev@hotmail.com>
2023-01-17 11:58:24 +0200

    Updated project

--- src/app/dog-breed-search/search-card/trait-search/range-input/range-input.component.ts
+++ src/app/dog-breed-search/search-card/trait-search/range-input/range-input.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RangeInputComponent implements OnInit {
 
+  minValue = 40;
+  maxValue = 60
+
   @Input() criteria: string = '';
 
   constructor() { }
@@ -14,4 +17,11 @@ export class RangeInputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return `${value}`;
+  }
 }
